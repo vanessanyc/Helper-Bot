3a5300717d9f6414b59fa23e780339f17ea05642
vanessanyc <elm.vanessa.louis@gmail.com>
2023-03-01 23:22:16 -0500

    edits

--- server/index.js
+++ server/index.js
@@ -26,20 +26,44 @@ io.on("connection", (socket) =>{
     })
 });
 
+const sentimentAnalyzer = new sentiment();
+const tokenizer = new natural.WordTokenizer();
+
 const handleMessage = (message, callback) => {  
+
     console.log('Received message:', message);
+
     if (typeof message.message === 'string'){
-        const greetings = ['hi', 'hello', 'hey', 'greetings'];
-        const goodbyes = ['bye', 'goodbye', 'see you', 'take care'];
+        const greetings = ['hi', 'hello', 'hey', 'greetings', 'whats up', "what's up", 'yo'];
+        const goodbyes = ['bye', 'goodbye', 'see you', 'take care', 'later'];
         const questions = ['who', 'what', 'where', 'when', 'why', 'how'];
+        const appreciations = ['thank you', 'thanks'];
     
-        let response = "I'm sorry, I don't understand. Can you please rephrase?";
+        const words = tokenizer.tokenize(message.message);
+        const sentimentScore = sentimentAnalyzer.analyze(message.message).score;
+
+        let response;
     
-        const words = message.message.split(' ');
+        //const words = message.message.split(' ');
+
+        if (sentimentScore < 0) {
+            response = 'I\'m sorry, it sounds like you\'re feeling down. How can I assist you?';
+        } 
+          
+        else if (sentimentScore > 0) {
+            response = 'Great! I\'m glad to hear that. How can I assist you?';
+        } 
+          
+        else {
+            response = 'Okay! How can I assist you?';
+        }
     
         // Check if the message is a greeting
         if (words.some(word => greetings.includes(word.toLowerCase()))) {
         response = 'Hello there! How can I assist you?';
+        }
+        else if(words.some(word => appreciations.includes(word.toLowerCase()))) {
+        response = "No problem, I'm happy to help! Feel free to ask any other questions."
         }
         // Check if the message is a goodbye
         else if (words.some(word => goodbyes.includes(word.toLowerCase()))) {
